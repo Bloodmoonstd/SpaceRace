9ec0e0ab27132c7c12a67fb79bd705442873d36b
John <John_comrie@hotmail.co.uk>
2016-05-25 17:11:56 +0100

    First commit

---
+++ core/src/com/spacerace/game/GameMenu.java
@@ -0,0 +1,17 @@
+package com.spacerace.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by John on 10/05/2016.
+ */
+public class GameMenu extends ApplicationAdapter {
+    SpriteBatch batch;
+
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+
+    }
+}
